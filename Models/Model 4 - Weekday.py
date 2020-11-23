35b35520830922163c5308b6bf015c55d6240e98
Yehuda Hecht <yhecht1@gmail.com>
2020-11-23 18:57:57 -0500

    Minor edits

--- Models/Model 4 - Weekday.py
+++ Models/Model 4 - Weekday.py
@@ -13,18 +13,23 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics import f1_score, confusion_matrix, make_scorer
 
+# Import data
+print('Importing data...')
 data_path = r'data/clean_df.csv.gz'
 df = pd.read_csv(data_path)
 
 # Transform datetime variables
 df['CRASH DATE'] = pd.to_datetime(df['CRASH DATE'])
 df['CRASH TIME'] = pd.to_datetime(df['CRASH TIME'])
+print('Done.')
 
 # Add target variable
+print('Setting target variable...')
 df['CASUALTIES?'] = 0
 mask = df['TOTAL PEDESTRIAN CASUALTIES'] != 0
 df.loc[mask, 'CASUALTIES?'] = 1
-df.loc[df['TOTAL PEDESTRIAN CASUALTIES'] != 1, ['TOTAL PEDESTRIAN CASUALTIES','CASUALTIES?']].sample    (5)
+df.loc[df['TOTAL PEDESTRIAN CASUALTIES'] != 1, ['TOTAL PEDESTRIAN CASUALTIES','CASUALTIES?']].sample(5)
+print('Done.')
 
 # Random Forest hyperparameters
 rf_params = {
@@ -49,17 +54,22 @@ kmeans_params = {
 }
 
 # Add K-means cluster features
+print('Adding cluster features...')
 clusters=52
 kmeans = KMeans(**kmeans_params)
 kmeans.fit(df[['LATITUDE','LONGITUDE']].values)
 df['CLUSTERS'] = kmeans.labels_
+print('Done.')
 
 # Add borough features
+print('Adding borough features...')
 borough_dummies = pd.get_dummies(df['BOROUGH'], sparse=True)
 cluster_dummies = pd.get_dummies(df['CLUSTERS'], prefix='CLUSTER', sparse=True)
 pre_X = cluster_dummies.join(borough_dummies)
+print('Done.')
 
 # Add year/month/season features
+print('Adding date/time features...')
 df['YEAR'] = df['CRASH DATE'].dt.year
 df['MONTH'] = df['CRASH DATE'].dt.month
 
@@ -71,15 +81,20 @@ df['HOUR'] = df['CRASH TIME'].dt.hour
 hour_dummies = pd.get_dummies(df['HOUR'], sparse=True, prefix='HOUR')
 
 pre_X = pre_X.join(df[['DURING DAYTIME','RUSH HOUR']]).join(hour_dummies)
+print('Done.')
 
 # Add weekday features
+print('Adding weekday features...')
 weekday_dummies = pd.get_dummies(df['WEEKDAY'], sparse=True)
 pre_X = pre_X.join(weekday_dummies)
+print('Done.')
 
 # Train-test split
+print('Splitting train/test data...')
 X = scipy.sparse.csr_matrix(pre_X)
 y = df['CASUALTIES?']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+print('Done.')
 
 # Modeling
 print('Creating logistic regression model...')
