3050c7c23016eee155c8fbb538a1205d0785f3c5
Yehuda Hecht <yhecht1@gmail.com>
2020-11-23 18:43:49 -0500

    Add Model 4 training scores

--- Models/Model 4 - Weekday.py
+++ Models/Model 4 - Weekday.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import datetime as dt
+import seaborn as sns
 import scipy.stats as stats
 import scipy.sparse
 from sklearn.model_selection import train_test_split, GridSearchCV
--- Models/Model 6 - OffStreetNames.py
+++ Models/Model 6 - OffStreetNames.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 import datetime as dt
+import seaborn as sns
 import scipy.stats as stats
 import scipy.sparse
 from sklearn.model_selection import train_test_split, GridSearchCV
